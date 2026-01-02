f551fb7d03dd8df9eb37c6e28c0116f2f91defb0
NanaYaw805 <arhinyaw64@gmail.com>
2026-01-02 07:59:43 +0000

    add: count query to search

--- src/main/java/com/warp5/warp5_construction_i/repositories/EquipmentRepository.java
+++ src/main/java/com/warp5/warp5_construction_i/repositories/EquipmentRepository.java
@@ -45,24 +45,34 @@ public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 
 
 
-        @Query("""
-        SELECT e FROM Equipment e
-        WHERE (:name IS NULL OR LOWER(e.name) LIKE LOWER(CONCAT('%', :name, '%')))
-          AND (:location IS NULL OR LOWER(e.location) = LOWER(:location))
-          AND (:minPrice IS NULL OR e.price >= :minPrice)
-          AND (:maxPrice IS NULL OR e.price <= :maxPrice)
-          AND (:minRating IS NULL OR e.rating >= :minRating)
-        ORDER BY e.location DESC
-    """)
-        Page<Equipment> searchEquipment(
-                @Param("name") String name,
-                @Param("location") String location,
-                @Param("minPrice") Double minPrice,
-                @Param("maxPrice") Double maxPrice,
-                @Param("minRating") Double minRating,
-                Pageable pageable
-        );
-    }
+    @Query(
+            value = """
+    SELECT e FROM Equipment e
+    WHERE (:name IS NULL OR LOWER(e.equipmentName) LIKE LOWER(CONCAT('%', :name, '%')))
+    AND (:location IS NULL OR LOWER(e.location) = LOWER(:location))
+    AND (:minPrice IS NULL OR e.priceRate IS NOT NULL AND e.priceRate >= :minPrice)
+    AND (:maxPrice IS NULL OR e.priceRate IS NOT NULL AND e.priceRate <= :maxPrice)
+    AND (:minRating IS NULL OR e.serviceRating IS NOT NULL AND e.serviceRating >= :minRating)
+    """,
+            countQuery = """
+    SELECT COUNT(e) FROM Equipment e
+    WHERE (:name IS NULL OR LOWER(e.equipmentName) LIKE LOWER(CONCAT('%', :name, '%')))
+    AND (:location IS NULL OR LOWER(e.location) = LOWER(:location))
+    AND (:minPrice IS NULL OR e.priceRate IS NOT NULL AND e.priceRate >= :minPrice)
+    AND (:maxPrice IS NULL OR e.priceRate IS NOT NULL AND e.priceRate <= :maxPrice)
+    AND (:minRating IS NULL OR e.serviceRating IS NOT NULL AND e.serviceRating >= :minRating)
+    """
+    )
+    Page<Equipment> searchEquipment(
+            @Param("name") String name,
+            @Param("location") String location,
+            @Param("minPrice") Double minPrice,
+            @Param("maxPrice") Double maxPrice,
+            @Param("minRating") Double minRating,
+            Pageable pageable
+    );
+
+}
 
 
 
